86be318e4b1674c7e120cdee553ef3af0588b20a
Nwaeze Destiny <your-email@example.com>
2026-06-02 15:00:19 -0700

    webhook clarified

---
+++ server/dist/server.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import express from 'express';
+import cors from "cors";
+import connectDB from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express';
+import { clerkWebhook } from "./controllers/webhooks.js";
+const app = express();
+// Connect to MongoDB
+connectDB();
+app.post('/api/clerk', express.raw({ type: "application/json" }), clerkWebhook);
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('Server is Live!');
+});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
